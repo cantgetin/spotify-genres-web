6b9456cb07b1284b360609e7204b9091cab944e5
cantgetin <84032933+cantgetin@users.noreply.github.com>
2023-02-11 22:00:34 +0500

    Add global styles, fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import {appAuthState} from "../store/store";
-    import { goto } from '$app/navigation';
-
+    import {goto} from '$app/navigation';
     const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
     const login = () => {
         let client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-        let redirect_uri = 'http://localhost:5173/authorized';
+        let redirect_uri = 'http://localhost:5173/authorize';
         let state = genRanHex(16);
         let scope = 'user-read-private user-read-email user-top-read playlist-read-private';
 
@@ -17,11 +16,35 @@
 
 </script>
 
-<div>
-    <h1>Welcome to Spotify Genres</h1>
-    <button on:click={login}>Click to authorize</button>
+<div class="app">
+    <div>
+        <h1>Welcome to Spotify Genres</h1>
+        <button on:click={login}>Click to authorize</button>
+    </div>
 </div>
 
 <style>
-
+    .app {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    h1 {
+        font-size: 48px;
+    }
+    button {
+        color: #121212;
+        display: block;
+        margin:20px auto;
+        font-size: 20px;
+        padding: 10px 20px;
+        background: #1ed760;
+        border: none;
+        border-radius: 25px;
+        cursor: pointer;
+    }
+    button:hover {
+        transform: scale(104%);
+    }
 </style>
--- src/routes/authorize/+page.svelte
+++ src/routes/authorize/+page.svelte
@@ -13,12 +13,22 @@
     // alert(appAuthState.getAppState())
 </script>
 
-<div>
+<div class="authorize">
     {#if isCorrectState}
-        <h1>authorized! wtf</h1>
-        <h2>Code: {code}</h2>
-        <h2>State: {state}</h2>
+        <h1>Authorized!</h1>
+<!--        <h2>Code: {code}</h2>-->
+<!--        <h2>State: {state}</h2>-->
     {:else}
-        <h1>Something went wrong, try again</h1>
+        <h1>Something went wrong, try again!</h1>
     {/if}
 </div>
+
+<style>
+    .authorize {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
